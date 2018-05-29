1baff9230c5201b945067c7b74812aa2fc50dbe3
luan.dbrito <luandb90@gmail.com>
2018-05-29 17:16:57 -0300

    Initial commit

---
+++ app/src/main/java/br/senac/sp/l13/terceiroapp/LogoActivity.java
@@ -0,0 +1,13 @@
+package br.senac.sp.l13.terceiroapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logo_layout);
+    }
+}
